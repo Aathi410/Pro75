764f9210eaecbd4bd206cbcd8d5335385e9249d7
Aathi410 <n4aathi04@gmail.com>
2020-10-26 12:27:04 +0530

    files added

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput,KeyboardAvoidingView,ToastAndroid } from 'react-native';
 import {Header} from 'react-native-elements';
 import db from '../config';
 
@@ -24,53 +24,56 @@ export default class WriteStoryScreen extends React.Component {
             author: '',
             storyText: '',
         })
+        ToastAndroid.show('The Story Is Sumitted', ToastAndroid.SHORT);
     }
 
     render(){
         return(
-            <View style={styles.container}>
-                <Header 
-                    backgroundColor = {'red'}
-                    centerComponent = {{
-                        text : 'Story Hub',
-                        style : { color: 'black', fontSize: 23, fontWeight: 'bold',}
-                    }}
-                />
-                <TextInput 
-                    placeholder="Story Title "
-                    onChangeText= {(text)=>{
-                        this.setState({
-                            title: text
-                        })
-                    }}
-                    value={this.state.title}
-                    style={styles.title}/>
-                <TextInput
-                    placeholder="Author "
-                    onChangeText= {(text)=>{
-                        this.setState({
-                            author: text
-                        })
-                    }}
-                    value={this.state.author}
-                    style={styles.author} />
-                <TextInput 
-                    placeholder="Write your story"
-                    onChangeText= {(text)=>{
-                        this.setState({
-                            storyText: text
-                        })
-                    }}
-                    value={this.state.storyText}
-                    style={styles.storyText}
-                    multiline={true}/>
+            <KeyboardAvoidingView style={styles.container} behavior = "padding" enabled>
+                <View>
+                    <Header 
+                        backgroundColor = {'red'}
+                        centerComponent = {{
+                            text : 'Story Hub',
+                            style : { color: 'black', fontSize: 23, fontWeight: 'bold',}
+                        }}
+                    />
+                    <TextInput 
+                        placeholder="Story Title "
+                        onChangeText= {(text)=>{
+                            this.setState({
+                                title: text
+                            })
+                        }}
+                        value={this.state.title}
+                        style={styles.title}/>
+                    <TextInput
+                        placeholder="Author "
+                        onChangeText= {(text)=>{
+                            this.setState({
+                                author: text
+                            })
+                        }}
+                        value={this.state.author}
+                        style={styles.author} />
+                    <TextInput 
+                        placeholder="Write your story"
+                        onChangeText= {(text)=>{
+                            this.setState({
+                                storyText: text
+                            })
+                        }}
+                        value={this.state.storyText}
+                        style={styles.storyText}
+                        multiline={true}/>
 
-                <TouchableOpacity
-                    style={styles.submitButton}
-                    onPress={this.submitStory}>
-                    <Text style={styles.buttonText}>Submit</Text>
-                </TouchableOpacity>
-            </View>
+                    <TouchableOpacity
+                        style={styles.submitButton}
+                        onPress={this.submitStory}>
+                        <Text style={styles.buttonText}>Submit</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
         );
     }
 }
